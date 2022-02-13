244653e517f7eafcdd28ed7ec295f7c3e43a9176
Jack Conroy <jackconroy777@gmail.com>
2022-02-13 11:06:05 +0000

    Setup base.html file, configre clinic app urls, create home view

--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """
+    Renders the main base.html template
+    """
+    return render(request, 'base.html')
