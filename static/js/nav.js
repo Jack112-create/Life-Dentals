61ef010d3a5401cd6ad840d7d7f138b3aa87d544
Jack Conroy <jackconroy777@gmail.com>
2022-02-13 13:41:08 +0000

    Add basic navbar to base.html

---
+++ static/js/nav.js
@@ -0,0 +1,8 @@
+const navToggle = document.getElementById('navToggle');
+const nav = document.getElementById('nav-links');
+
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    console.log('click')
+})
