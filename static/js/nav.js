b63f85471d24a7bdc6e0122611ecdcafe9fae1ee
Jack Conroy <jackconroy777@gmail.com>
2022-02-13 16:22:25 +0000

    Add nav links hover effect

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,7 +1,6 @@
 const navToggle = document.getElementById('navToggle');
 const nav = document.getElementById('nav-links');
 
-
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav-open');
     console.log('click')
