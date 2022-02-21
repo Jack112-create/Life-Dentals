b05f9f9c76c0d4ee820501e1513f16ebaf008552
Jack Conroy <jackconroy777@gmail.com>
2022-02-21 17:11:05 +0000

    Setup treatments app - templates, urls and view

--- lifedentals/urls.py
+++ lifedentals/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clinic.urls')),
+    path('treatments/', include('treatments.urls'))
 ]
