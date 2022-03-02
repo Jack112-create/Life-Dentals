d4f23e3b08244e14e13bb95f01186ebadbcfab10
Jack Conroy <jackconroy777@gmail.com>
2022-03-02 11:31:42 +0000

    Add Update testimonial markup and add testimonial functionality

--- treatments/static/js/treatments.js
+++ treatments/static/js/treatments.js
@@ -1,34 +1,34 @@
-const testimonials = [
-    {
-        name: "-(Caitlin)",
-        message: "Everybody was extremely kind and made me feel comfortable throughout my entire treatment process."
-    },
-    // {
-    //     name: "-(John)",
-    //     message: "The professionals at Life Dentals explained my treatment process very clearly to me. This gave me the confidence to go ahead with my treatment."
-    // },
-    // {
-    //     name: "-(Barry)",
-    //     message: "Great customer experience! The team did a great job at giving me my dream smile with no pain involved!"
-    // }
-]
+let testimonials = document.getElementsByClassName('testimonial-content');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const totalTestimonials = testimonials.length;
+let index = 0;
+console.log(testimonials);
 
-function createTestimonials() {
-
-    for (let testimonial of testimonials) {
-    let testimonialText = document.createElement('p');
-    testimonialText.innerText = testimonial.message;
-    testimonialText.classList.add("testimonial-text");
-
-    let testimonialPerson = document.createElement('span');
-    testimonialPerson.innerText = testimonial.name;
-    testimonialPerson.classList.add("testimonial-person");
-
-    let testimonialSection = document.getElementsByClassName('testimonial')[0];
-    testimonialSection.appendChild(testimonialText);
-    testimonialSection.appendChild(testimonialPerson);
+function nextTestimonial(direction) {
+    if(direction == 'next') {
+      index++;
+      if(index == totalTestimonials) {
+        index = 0;
+      }
+    } else {
+      if(index == 0) {
+        index = totalTestimonials - 1;
+      } else {
+        index--;
+      }
     }
-
+  
+    for(let i = 0; i < testimonials.length; i++) {
+      testimonials[i].classList.remove('show-testimonial');
+    }
+    testimonials[index].classList.add('show-testimonial');
 }
 
-createTestimonials()
+prev.addEventListener('click', () => {
+  nextTestimonial('next');
+})
+
+next.addEventListener('click', () => {
+  nextTestimonial('prev');
+})
