ed25761bc375605dd5bd0de5137fc96e28e4a2ea
Jack Conroy <jackconroy777@gmail.com>
2022-03-10 16:08:26 +0000

    Update custom user form and added styling to sign up form

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,18 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 
 
 class CustomUserCreationForm(UserCreationForm):
-   class Meta:
-       model = User
-       fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+        help_texts = {
+            'username': None
+        }
