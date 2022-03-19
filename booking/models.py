8510ef81caae5d23b9b2bda822bb7660951d8a67
Jack Conroy <jackconroy777@gmail.com>
2022-03-19 12:21:38 +0000

    Applied new migrations to Booking model

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from treatments.models import Treatment
+from datetime import datetime, date
 
 
 class Booking(models.Model):
 
     TREATMENTS_LIST = []
+
+    TIMESLOT_LIST = (
+        (0, '09:00 – 09:30'),
+        (1, '10:00 – 10:30'),
+        (2, '11:00 – 11:30'),
+        (3, '12:00 – 12:30'),
+        (4, '13:00 – 13:30'),
+        (5, '14:00 – 14:30'),
+        (6, '15:00 – 15:30'),
+        (7, '16:00 – 16:30'),
+        (8, '17:00 – 17:30'),
+    )
 
     # Retrieve all instances of Treatment model
     TREATMENT_CHOICES = Treatment.objects.all()
@@ -16,6 +29,8 @@ class Booking(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     treatments = models.CharField(max_length=15, choices=TREATMENTS_LIST)
+    booking_date = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    booking_time = models.CharField(max_length=50, choices=TIMESLOT_LIST, default=TIMESLOT_LIST[0][1])
 
     def __str__(self):
         return str(f'{self.user}: booking')
