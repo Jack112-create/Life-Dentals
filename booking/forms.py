4a4815daf190fb32cdd46d79326ea37e4d71d8e7
Jack Conroy <jackconroy777@gmail.com>
2022-03-21 14:28:43 +0000

    Add new form to create datepicker widget and add to model form

--- booking/forms.py
+++ booking/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Booking
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class BookingForm(ModelForm):
 
     class Meta:
         model = Booking
         fields = ['treatments', 'booking_time', 'booking_date']
+        widgets = {
+            'booking_date': DateInput()
+        }
