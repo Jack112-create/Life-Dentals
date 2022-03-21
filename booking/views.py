38fe2a127b86ecf825e9ef58a00cb0cf11b18d20
Jack Conroy <jackconroy777@gmail.com>
2022-03-21 11:11:04 +0000

    Add logic to check for clashing bookings and add error messages to template

--- booking/views.py
+++ booking/views.py
@@ -1,30 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Booking
 from .forms import BookingForm
+from django.contrib import messages
 
 def booking(request):
     user = request.user
     print(user)
     
-    # print(bookings)
-
     form = BookingForm()
+    
 
     if request.method == 'POST':
         form = BookingForm(request.POST)
 
-        my_time = 0
-        bookings = Booking.objects.filter(booking_time=my_time)
-        print(bookings)
+        selected_time = request.POST['booking_time']
+        selected_date = request.POST['booking_date']
 
-        if len(bookings) == 0:
-            print(len(bookings))
+        booked_slots = Booking.objects.filter(booking_time=selected_time)
+        booked_dates = Booking.objects.filter(booking_date=selected_date)
+        print(booked_dates)
+        
+        if len(booked_dates) > 0:
+            for slot in booked_dates:
+                if slot.booking_time == selected_time:
+                    print('Cannot book that time!')
+                    messages.error(request, 'Cannot book that time!')
+                    context = {
+                        'form': form
+                    }
+                    return render(request, 'booking/booking.html', context)
+
+                else:
+                    if form.is_valid():
+                        booking = form.save(commit=False)
+                        booking.user = request.user
+                        booking.save()
+                        print('Booking confirmed')
+        else:
             if form.is_valid():
                 booking = form.save(commit=False)
                 booking.user = request.user
                 booking.save()
-        else:
-            print('Cannot book that time!')
+                print('Booking confirmed')
+        
+
+        # if len(bookings_dates) == 0:
+            
+        #     if form.is_valid():
+        #         booking = form.save(commit=False)
+        #         booking.user = request.user
+        #         booking.save()
+        #         print('Booking confirmed')
+        # else:
+        #     print('Cannot book that time!')
 
     context = {
         'form': form
