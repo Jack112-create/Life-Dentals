0032dc67de06599814effb207f49a2eda9d02dc1
Jack Conroy <jackconroy777@gmail.com>
2022-04-01 12:07:56 +0000

    Remove console log from nav.js and add comment for toggle functionality

--- static/js/nav.js
+++ static/js/nav.js
@@ -2,6 +2,6 @@ const navToggle = document.getElementById('navToggle');
 const nav = document.getElementById('nav-links');
 
 navToggle.addEventListener('click', () => {
+    // Toggle navbar to open and close
     nav.classList.toggle('nav-open');
-    console.log('click')
 })
