61d0cb9446def436e3e8b83753e84fe02e69a6ca
Jack Conroy <jackconroy777@gmail.com>
2022-06-13 19:55:04 +0000

    Fix user being able to book same time slot in edit booking form

--- booking/views.py
+++ booking/views.py
@@ -39,6 +39,7 @@ def createBooking(request):
         
         # Check to see if there are bookings for the same day
         if len(booked_dates) > 0:
+            print('same day')
             for slot in booked_dates:
                 # Check if the user seleted time is already booked and return error message
                 if slot.booking_time == selected_time:
@@ -51,6 +52,8 @@ def createBooking(request):
                         # Assign the user with the booking
                         booking.user = request.user
                         booking.save()
+                        messages.success(request, 'Booking Successful!')
+                        return redirect('booking')
         else:
             # Check if the form data is valid
             if form.is_valid():
@@ -70,16 +73,43 @@ def editBooking(request):
     user = request.user
 
     booking = Booking.objects.get(user=user)
-    print(booking)
 
     form = BookingForm(instance=booking)
 
     if request.method == 'POST':
-        print('hi')
-        form = BookingForm(request.POST, instance=booking)
-        if form.is_valid():
-            form.save()
-            return redirect('booking')
+        # Get user selected time and date for booking
+        selected_time = request.POST['booking_time']
+        selected_date = request.POST['booking_date']
+
+        # Query booking table to find time & date that match user selected time and date
+        booked_slots = Booking.objects.filter(booking_time=selected_time)
+        booked_dates = Booking.objects.filter(booking_date=selected_date)
+        print(booked_dates)
+
+        # Check to see if there are bookings for the same day
+        timeList = []
+        for slot in booked_slots:
+            timeList.append(slot.booking_time)
+
+        if len(booked_dates) > 0:
+
+            if selected_time not in timeList:
+                print('not in')
+                form = BookingForm(request.POST, instance=booking)
+                if form.is_valid():
+                    form.save()
+                    return redirect('booking')
+            
+            else:
+                print('is in')
+                messages.error(request, 'Cannot book that time!')
+
+        else:
+            form = BookingForm(request.POST, instance=booking)
+            if form.is_valid():
+                form.save()
+                return redirect('booking')
+
 
     context = {
         'form': form
