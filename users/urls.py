19fb462643caea45b6370e1ff7d40c2747dc2eca
Jack Conroy <jackconroy777@gmail.com>
2022-06-27 19:52:37 +0000

    format user model and urls file

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register', views.registerUser, name="register")
-
 ]
